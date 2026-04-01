6db6fb31929f7bc86b7bdfa7b690143eb210ea01
biee202 <adlyfarabi@gmail.com>
2026-04-01 22:18:47 +0700

    Update and rename main.py to fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,4 @@
-for i in range(1, 100):
+for i in range(1, 101):
     if i % 3 == 0 and i % 5 == 0:
         print("fizzbuzz")
     elif i % 3 == 0:
@@ -7,3 +7,4 @@ for i in range(1, 100):
         print("buzz")
     else:
         print(i)
+
